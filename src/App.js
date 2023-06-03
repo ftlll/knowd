6db05a907e509fe6ed7ff468f3666ad3020e8308
Jian <jian.liao1@ucalgary.ca>
2023-06-03 10:59:29 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Stripe, Gmail, Alexa} from "./dataset";
+import {Engine, Search} from "./search-engine";
+
+// Here's the dataset that neeed to be used indexed for the search engine
+const dataset = [...Stripe, ...Gmail, ...Alexa].map((text) => ({text}));
+const engine = Engine(dataset);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Knowd Technical Interview - Novelist
+                </p>
+                <button onClick={() => console.log(Search(engine, "email").map((item) => ({text: dataset[item[0]].text, similarity: item[1]})))}>
+                    Search
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
